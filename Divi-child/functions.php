fd293ddb8ea9121854887580723fb88a0954bb64
Jonas Hjalmarsson <Jonas.Hjalmarsson@hultsfred.se>
2021-04-16 11:02:31 +0200

    Minor style reorganisation
    moved some styles from a plugin to child theme.
    Also added white-bg-black-text

--- Divi-child/functions.php
+++ Divi-child/functions.php
@@ -3,7 +3,7 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_script( 'rek-ai', "https://static.rek.ai/560d434f.js");
     wp_enqueue_script( 'rek-ai-autocomplete', "https://static.rek.ai/addon/rekai_autocomplete.min.js", array("rek-ai"));
-    wp_enqueue_script( 'hultsfred-custom', get_stylesheet_directory_uri() . "/custom.js", array( 'jquery', 'rek-ai-autocomplete' ), "1.0");
+    wp_enqueue_script( 'hultsfred-custom', get_stylesheet_directory_uri() . "/custom.js", array( 'jquery', 'rek-ai-autocomplete' ), "1.1");
 
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
